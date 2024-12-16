45b852610cea0a4831096e3c9a9ed62c2cc12fd8
Muhammad Rafi Walidain <143930261+Rafiwalidain@users.noreply.github.com>
2024-12-16 21:15:57 +0700

    nambah struktur baru

--- system event manager.c
+++ system event manager.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct event {
+  char nama[50];
+  char tanggal[20];
+  char deskripsi[1000];
+  struct event* next;
+} event;
+event* event_list = NULL;
+
+typedef struct queue {
+  event* depan;
+  event* belakang;
+} queue;
+queue event_queue = {NULL, NULL};
+
+typedef struct stack {
+  event* top;
+} stack;
+stack undo_stack = {NULL};
